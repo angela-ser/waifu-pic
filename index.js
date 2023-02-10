ea907470ed752d0eac59e8830fb9688072d85a70
Angela <104827062+angela-ser@users.noreply.github.com>
2023-02-10 19:41:15 +0330

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    message: "This is a server for get waifu pics. All rights reserved for ® HyNO",
+    message: "This is a server for get waifu pics. All rights reserved for ® HyNO\n\nApi Help:\sfw Category: waifu , neko , shinobu , bully , cry , hug , kiss , lick , pat , smug , highfive , nom , bite , slap , wink , poke , dance , cringe , blush , happy\nsfw Category: waifu , neko , trap , blowjob\n\sfw example: https://api-hyno-waifu.vercel.app/sfw/waifu\nsfw example: https://api-hyno-waifu.vercel.app/nsfw/waifu",
   });
 });
 
