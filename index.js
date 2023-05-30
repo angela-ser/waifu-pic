07f8e207b58d49951296d7475b8e8b7de9f1fba4
Angela <104827062+angela-ser@users.noreply.github.com>
2023-05-30 10:59:45 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+
 const cors = require("cors");
+
 const got = require("got");
 
 const ENDPOINTS = require("./lib/endpoints.js");
@@ -9,67 +11,15 @@ const API_URL = "https://api.waifu.pics";
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
+
   res.json({
+
     message: "This is a server for get waifu pics. All rights reserved for ® HyNO                 Api Help:
 sfw Category: waifu , neko , shinobu , bully , cry , hug , kiss , lick , pat , smug , highfive , nom , bite , slap , wink , poke , dance , cringe , blush , happy                             Nsfw Category: waifu , neko , trap , blowjob                   sfw example: https://api-hyno-waifu.vercel.app/sfw/waifu\                         nsfw example: https://api-hyno-waifu.vercel.app/nsfw/waifu\",
-  });
-});
-
-app.get("/:type/:endpoint", async (req, res) => {
-  let endpoint = req.params.endpoint.toLocaleLowerCase();
-  let type = req.params.type.toLocaleLowerCase();
-  res.set("Cache-Control", "no-cache");
-  if (ENDPOINTS[type].includes(endpoint)) {
-    fetchImage(type, endpoint, res);
-  } else if (endpoint === "random") {
-    endpoint =
-      ENDPOINTS[type][Math.floor(Math.random() * ENDPOINTS[type].length)];
-    fetchImage(type, endpoint, res);
-  } else {
-    res.status(400).json({
-      message: "Bad endpoint",
-    });
-  }
-});
-
-async function fetchImage(type, endpoint, response) {
-  try {
-    const { url } = await got(`${API_URL}/${type}/${endpoint}`).json();
-
-    got
-      .stream(url)
-      .on("response", (response) => {
-        response.headers["cache-control"] = "no-cache";
-      })
-      .pipe(response);
-  } catch (error) {
-    response.status(500).json({
-      message: error.message,
-    });
-  }const express = require("express");
-
-const cors = require("cors");
-
-const got = require("got");
-
-const ENDPOINTS = require("./lib/endpoints.js");
-
-const API_URL = "https://api.waifu.pics";
-
-const app = express();
-
-app.use(cors());
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-
-  res.json({
-
-    message: "This is a simple server to get images from waifu.pics",
 
   });
 
@@ -143,11 +93,4 @@ app.listen(PORT, () => {
 
   console.log("Server is listening in port" + PORT);
 
-});
-}
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-  console.log("Server is listening in port" + PORT);
 });
