774493dd00ce510a47eb7d5540d1397e05daa021
Angela <104827062+angela-ser@users.noreply.github.com>
2023-05-30 11:31:26 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,98 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const cors = require("cors");
 
-const PORT = 4000
+const got = require("got");
+
+const ENDPOINTS = require("./lib/endpoints.js");
+
+const API_URL = "https://api.waifu.pics";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+
+  res.send({
+
+    message: "This is a simple server to get images from waifu.pics",
+
+  });
+
+});
+
+app.get("/:type/:endpoint", async (req, res) => {
+
+  let endpoint = req.params.endpoint.toLocaleLowerCase();
+
+  let type = req.params.type.toLocaleLowerCase();
+
+  res.set("Cache-Control", "no-cache");
+
+  if (ENDPOINTS[type].includes(endpoint)) {
+
+    fetchImage(type, endpoint, res);
+
+  } else if (endpoint === "random") {
+
+    endpoint =
+
+      ENDPOINTS[type][Math.floor(Math.random() * ENDPOINTS[type].length)];
+
+    fetchImage(type, endpoint, res);
+
+  } else {
+
+    res.status(400).json({
+
+      message: "Bad endpoint",
+
+    });
+
+  }
+
+});
+
+async function fetchImage(type, endpoint, response) {
+
+  try {
+
+    const { url } = await got(`${API_URL}/${type}/${endpoint}`).json();
+
+    got
+
+      .stream(url)
+
+      .on("response", (response) => {
+
+        response.headers["cache-control"] = "no-cache";
+
+      })
+
+      .pipe(response);
+
+  } catch (error) {
+
+    response.status(500).json({
+
+      message: error.message,
+
+    });
+
+  }
+
+}
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
 
-  console.log(`API listening on PORT ${PORT} `)
+  console.log("Server is listening in port" + PORT);
 
-})
-
-app.get('/', (req, res) => {
-
-  res.send('Hey this is my API running 🥳')
-
-})
-
-app.get('/about', (req, res) => {
-
-  res.send('This is my about route..... ')
-
-})
+});
 
 // Export the Express API
 
